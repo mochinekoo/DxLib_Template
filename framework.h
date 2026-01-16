423d19707fa7fdf13da5151d2e816fa13d2796ec
mochineko <mochinekoo0113@gmail.com>
2026-01-16 16:49:59 +0900

    [change] inline定義にしました

--- framework.h
+++ framework.h
@@ -1,10 +1,10 @@
 #pragma once
 
 namespace GameScreen {
-	const char* TITLE = "Game"; //ウインドウのタイトル
-	const int WIDTH = 1280; //画面の幅
-	const int HEIGHT = 720; //画面の高さ
-	const int COLORBIT = 32; //カラービット
-	const int BACKGROUND[3] = {0, 0, 0}; //画面の背景
-	const int FPS = 60; //画面のフレームレート
+	inline const char* TITLE = "Game"; //ウインドウのタイトル
+	inline const int WIDTH = 1280; //画面の幅
+	inline const int HEIGHT = 720; //画面の高さ
+	inline const int COLORBIT = 32; //カラービット
+	inline const int BACKGROUND[3] = {0, 0, 0}; //画面の背景
+	inline const int FPS = 60; //画面のフレームレート
 }
